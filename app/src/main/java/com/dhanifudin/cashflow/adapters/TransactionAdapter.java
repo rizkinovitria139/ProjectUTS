eaee1a8294fd90aba3036854776fdcae3f0ec0e4
kiki <rahmarizki37@gmail.com>
2020-03-15 02:36:22 -0700

    menambahkan permbeda berupa warna abu untuk pemilihan jenis debit

--- app/src/main/java/com/dhanifudin/cashflow/adapters/TransactionAdapter.java
+++ app/src/main/java/com/dhanifudin/cashflow/adapters/TransactionAdapter.java
@@ -1,5 +1,6 @@
 package com.dhanifudin.cashflow.adapters;
 
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -38,6 +39,9 @@ public class TransactionAdapter extends RecyclerView.Adapter<TransactionAdapter.
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         Transaction item = items.get(position);
+        if(item.getType() == Transaction.Type.DEBIT){
+            holder.linearLayour.setBackgroundColor(Color.parseColor("#E7E6E6"));
+        }
         holder.bind(position, item);
     }
 
@@ -49,11 +53,13 @@ public class TransactionAdapter extends RecyclerView.Adapter<TransactionAdapter.
     public class ViewHolder extends RecyclerView.ViewHolder {
         TextView descriptionText;
         TextView amountText;
+        public View linearLayour;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             descriptionText = itemView.findViewById(R.id.text_description);
             amountText = itemView.findViewById(R.id.text_amount);
+            linearLayour = itemView.findViewById(R.id.linearLayout);
         }
 
         public void bind(final int index, final Transaction item){
