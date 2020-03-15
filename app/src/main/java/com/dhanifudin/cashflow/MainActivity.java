722bc45366b3d707911e632df48a6f318df021c6
kiki <rahmarizki37@gmail.com>
2020-03-15 03:52:18 -0700

    logika untuk logout

--- app/src/main/java/com/dhanifudin/cashflow/MainActivity.java
+++ app/src/main/java/com/dhanifudin/cashflow/MainActivity.java
@@ -115,6 +115,14 @@ public class MainActivity extends AppCompatActivity
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Intent intent = new Intent(this, SettingActivity.class);
+            startActivity(intent);
+            return true;
+        }else  if(id == R.id.action_logout){
+            session.logout();
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+            finish();
             return true;
         }
 
