c83c715cf890d355b820ce8e2eb93c545b348f9e
kiki <rahmarizki37@gmail.com>
2020-03-15 03:38:29 -0700

    extends PreferenceFragmentCompat implements method onCreatePreferences

--- app/src/main/java/com/dhanifudin/cashflow/SettingsFragment.java
+++ app/src/main/java/com/dhanifudin/cashflow/SettingsFragment.java
@@ -15,12 +15,12 @@ import android.view.ViewGroup;
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link Setting.OnFragmentInteractionListener} interface
+ * {@link SettingsFragment.OnFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link Setting#newInstance} factory method to
+ * Use the {@link SettingsFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class Setting extends PreferenceFragmentCompat {
+public class SettingsFragment extends PreferenceFragmentCompat {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -32,7 +32,7 @@ public class Setting extends PreferenceFragmentCompat {
 
     private OnFragmentInteractionListener mListener;
 
-    public Setting() {
+    public SettingsFragment() {
         // Required empty public constructor
     }
 
@@ -42,11 +42,11 @@ public class Setting extends PreferenceFragmentCompat {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment Setting.
+     * @return A new instance of fragment SettingsFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static Setting newInstance(String param1, String param2) {
-        Setting fragment = new Setting();
+    public static SettingsFragment newInstance(String param1, String param2) {
+        SettingsFragment fragment = new SettingsFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -65,14 +65,14 @@ public class Setting extends PreferenceFragmentCompat {
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
-
+        setPreferencesFromResource(R.xml., rootKey);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_setting, container, false);
+        return inflater.inflate(R.layout.fragment_settings, container, false);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
